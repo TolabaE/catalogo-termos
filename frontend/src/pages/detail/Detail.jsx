d1caf4b656cac477fd873fcfdfb1f6bdee07e87d
Eduardo Tolaba <eduardoetolaba@gmail.com>
2025-01-06 11:27:27 -0300

    correcion de estructura en la tarjeta de producto

--- frontend/src/pages/detail/Detail.jsx
+++ frontend/src/pages/detail/Detail.jsx
@@ -9,12 +9,22 @@ const Detail = () => {
 
     const {id_card} = useParams()//recibo el id por parametro de acuerdo al id de la tarjeta seleccionada
     const [product, setProduct] = useState({});
+    const [dynamicImg,setDynamicImg] = useState({})//creo un estado para guardar la imagen que se este seleccionando.
+    // const [position,setPosition] = useState(0);//creo un estado para manejar la posicion de la imagen cuando se realize el click
+
+    //una funcion que estera el evento click, y recibe por parametro la posicion de la imagen clickeada.
+    const changeImage = (id_image) =>{
+        //con la posicion busca el id de la imagen y cambia el estado de  la imagen que se muestra por pantalla.
+        const img = product.lista_imagen?.find(image => image.id_img === id_image);
+        setDynamicImg(img)
+    }
 
     useEffect(() => {
         const getAllProducts = () =>{
-        const foundProduct = termos.find(item => item.id_producto === parseInt(id_card));
+        const foundProduct = termos.find(item => item.id_producto === parseInt(id_card));//usamos el metodo find para buscar el producto con el id que recibo por parametro.
         if(foundProduct){
             setProduct(foundProduct)
+            setDynamicImg(foundProduct.lista_imagen[0])//la imagen que se muestre por defecto sea la primera.
         }else{
             console.log("producto no encontrado");
         }
@@ -30,12 +40,12 @@ const Detail = () => {
             <div className='detail__container'>
                 <div className='image__detail-container'>
                     <div className='image__main-detail'>
-                        <img src="https://stanley1913store.com/cdn/shop/products/LegendaryClassicBottle_1_1080x.png?v=1687972296" alt="" />                                                                         
+                        <img src={ dynamicImg.image} alt={dynamicImg.alt} />                                                                         
                     </div>
                     <div className='image__list-container'>
                         {
                             product.lista_imagen?.map(img =>(
-                                <div className='box__image-list' key={img.id_img}>
+                                <div className='box__image-list' key={img.id_img} onClick={()=>{changeImage(img.id_img)}}>
                                         <img src={img.image} alt={img.alt} />
                                 </div>
                             ))
@@ -45,9 +55,9 @@ const Detail = () => {
                 <div className='description__detail-container'>
                     <h2 className='title__detail'>{product.nombre}</h2>
                     <p> {product.detalle}</p>
-                    <b>{product.precio}</b>
-                    <i>stock disponible {product.stock}</i>
-                    <div>Comprar</div>
+                    <b>Precio $ {product.precio}</b>
+                    <i>{product.stock >=1 ? "stock disponibles" : "producto agotado"} </i>
+                    <div className='btn-detail'><a href="https://wa.me/3874830025?text=hola%20me%20interesa%20el%20termo%20stanley"  target='_blank' >COMPRAR</a></div>
                 </div>
             </div>
         </div>
--- frontend/src/pages/products/Products.jsx
+++ frontend/src/pages/products/Products.jsx
@@ -31,7 +31,6 @@ const Products = () => {
                                 />
                             ))
                         }
-                        
                     </div>
                 </div>
             </main>
